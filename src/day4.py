f49f21501656914fe87813d50ad12687fee52674
trevalkov <rodrigolf080@gmail.com>
2021-12-09 23:55:03 +0000

    .

--- src/day4.py
+++ src/day4.py
@@ -1,32 +1,29 @@
-
-
-
-
-
 def solve_part1(input_ls: list) -> int:
     score = 0
-    bingo = list(input_ls[0].split(" "))
-    _boards = input_ls[2:]
+    bingo = list(x for x in input_ls[0] if x != ',' and x != ' ')
+    _boards = list(board for board in input_ls[2:])
     boards = list()
-    for board in _boards.split("\n\n"):
-        win_cases = dict()
-        for num in board.split(" "):
-            win_cases[num] = False
-        boards.append(win_cases)
-    print(bingo)
-    # for num in bingo:
-    #     for board in boards:
-    #         for win_case in board:
-    #             if num in win_case.keys():
-    #                 win_case[num] = True
-    #             state = True
-    #             for s in win_case.values():
-    #                 if s == False:
-    #                     state = s
-    #             if state == True:
-    #                 for num in win_case.keys():
-    #                     score += num
+    new_board = list()
 
+    for line in _boards:
+        if line != "":
+            new_board.append(line)
+        else:
+            _boards.append(new_board)
+            new_board = list()
+
+    for board in _boards:
+        win_cases = dict()
+        for line in board:
+            for num in line.split():
+                win_cases[num] = False
+            boards.append(win_cases)
+
+    for num in bingo:
+        for board in boards:
+            print(board)
+            for win_cases in board:
+                continue
     return score
     
 
