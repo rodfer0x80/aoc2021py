0fa4c52034d3c4b47fdcf9c0ead2f632aee4ccaa
trevalkov <rodrigolf080@gmail.com>
2021-12-09 18:17:02 +0000

    day5

--- src/day5.py
+++ src/day5.py
@@ -5,39 +5,38 @@ def format_vents(input_ls: list) -> list:
     return list(vent.split(" -> ") for vent in input_ls)
 
 
-def cordinate(obj):
-    # Convert the co-ordinates from string format to int format
+def cordinate(obj: str) -> int:
     return tuple(map(int, obj.split(',')))
 
 
-def draw_horizontal_line(grid, y, x, x1):
+def draw_horizontal_line(grid: dict, y: int, x: int, x1: int) -> int:
     intersections = 0
-    # Range is inclusive of both ends i.e., min to max
     for i in range(min(x, x1), max(x, x1) + 1):
-        grid[(i, y)] += 1
-        
-        # Once we find a intersection, we increment the intersections counter
+        grid[(i, y)] += 1      
         if grid[(i, y)] == 2:
             intersections += 1
     return intersections
 
 
-def draw_vertical_line(grid, x, y, y1):
+def draw_vertical_line(grid: dict, x: int, y: int, y1: int) -> int:
     intersections = 0
-    # Range is inclusive of both ends i.e., min to max
     for i in range(min(y, y1), max(y, y1) + 1):
         grid[(x, i)] += 1
-
-        # Once we find a intersection, we increment the intersections counter
         if grid[(x, i)] == 2:
             intersections += 1
     return intersections
 
 
-def solve_part2(input_ls: list) -> int:
-    lines = format_vents(input_ls)
-
-    return 0
+def draw_diagonal_line(grid: dict, x: int, y: int, x1: int, y1: int) -> int:
+    counter = 0
+    upto = abs(x1 - x)
+    for _ in range(upto + 1):
+        grid[(x, y)] += 1
+        if grid[(x, y)] == 2:
+            counter += 1
+        x += 1 if x1 > x else -1
+        y += 1 if y1 > y else -1
+    return counter
 
 
 def solve_part1(input_ls: list) -> int:
@@ -55,6 +54,23 @@ def solve_part1(input_ls: list) -> int:
     return counter
 
 
+def solve_part2(input_ls: list) -> int:
+    lines = format_vents(input_ls)
+    grid = collections.defaultdict(int)
+    counter = 0
+
+    for line in lines:
+        p1, p2 = tuple(map(cordinate, line))
+        if p1[0] == p2[0]:
+            counter += draw_vertical_line(grid, p1[0], p1[1], p2[1])
+        elif p1[1] == p2[1]:
+            counter += draw_horizontal_line(grid, p1[1], p1[0], p2[0])
+        else:
+            counter += draw_diagonal_line(grid, p1[0], p1[1], p2[0], p2[1])
+            
+    return counter
+
+    
 def solve_day5(input_ls: list) -> list:
     output = list()
 
