398334cc4527d68a19f4dca6ef5439c3cfa599f9
trevalkov <rodrigolf080@gmail.com>
2021-12-09 18:00:20 +0000

    day8p2-logicalproblem

--- src/day8.py
+++ src/day8.py
@@ -9,12 +9,83 @@ def format__list(xs: list, delim: str) -> list:
 def decode_pattern(digit: str) -> int:
     if len(digit) == 2:
         return 1
-    if len(digit) == 4:
+    elif len(digit) == 4:
         return 4
-    if len(digit) == 3:
+    elif len(digit) == 3:
         return 7
-    if len(digit) == 7:
+    elif len(digit) == 7:
         return 8
+    else:
+        return -1
+
+# @lru_cache(maxsize=256)
+# def decode_pattern(digit: str) -> int:
+#     if len(digit) == 2:
+#         return 1
+#     elif len(digit) == 4:
+#         return 4
+#     elif len(digit) == 3:
+#         return 7
+#     elif len(digit) == 7:
+#         return 8
+#     else:
+#         return digit
+
+
+@lru_cache(maxsize=256)
+def deduce_pattern(digits: str, patterns: list) -> int:
+    decoded_patterns = []
+    for pattern in patterns.split(" "):
+        decoded_digit = decode_pattern(pattern)
+        if decoded_digit != -1:
+            decoded_patterns.append([decoded_digit, pattern])
+    decoded_patterns = sorted(decoded_patterns)
+    one = decoded_patterns[0][1]
+    four = decoded_patterns[1][1]
+    seven = decoded_patterns[2][1]
+    eight = decoded_patterns[3][1]
+
+    # find a
+    a = seven
+    for digit in decoded_patterns[0][1]:
+        a = a.replace(digit, '')
+
+    # find c and f
+    cf = one
+    c = cf[:1]
+    f = cf[1:]
+    six = eight.replace(c, '')
+    if len(six) == len(eight):
+        tmp = f
+        f = c
+        c = tmp
+        six = eight.replace(f, '')
+    # find d
+    arr = []
+    for pattern in patterns.split(" "):
+        if len(pattern) == 6:
+            arr.append(pattern)
+    zero  = arr[0]
+    nine = arr[2]
+    d = nine
+    for digit in zero:
+        d = d.replace(digit, '')
+    # find e
+    e = eight
+    for digit in nine:
+        e = e.replace(digit, '')
+
+    # find b
+    b = four
+    b = b.replace(d, '')
+    b = b.replace(f, '')
+    b = b.replace(c, '')
+    print(b)
+    # from this point we have a,c,d,e,f
+
+
+
+    return 0
 
 
 def count_four_known_digits(xs: list) -> list:
@@ -32,10 +103,21 @@ def count_four_known_digits(xs: list) -> list:
 def solve_part1(input_ls: list) -> int:
     xs = format__list(input_ls, " | ")
     return count_four_known_digits(xs)
-    
+
+            
 
 def solve_part2(input_ls: list) -> int:
-    return 0
+    xs = format__list(input_ls, " | ")
+    patterns = list()
+    digits = list()
+    total = 0
+    for x in xs:
+        patterns.append(x[0])
+        digits.append(x[1])
+    for i in range(len(digits)):
+        n = deduce_pattern(digits[i], patterns[i])
+        total += n
+    return total
 
 
 def solve_day8(input_ls: list) -> list:
