eadbac44b8a0f59843d229481f65aab580b6124b
trevalkov <rodrigolf080@gmail.com>
2021-12-10 00:33:47 +0000

    .

--- src/day4.py
+++ src/day4.py
@@ -1,28 +1,28 @@
 def solve_part1(input_ls: list) -> int:
     score = 0
     bingo = list(x for x in input_ls[0] if x != ',' and x != ' ')
-    _boards = list(board for board in input_ls[2:])
+    boards2 = list(board for board in input_ls[2:])
+    boards1 = list()
     boards = list()
     new_board = list()
-
-    for line in _boards:
+    
+    for line in boards2:
         if line != "":
             new_board.append(line)
         else:
-            _boards.append(new_board)
+            print(new_board)
+            boards1.append(new_board)
             new_board = list()
 
-    for board in _boards:
-        win_cases = dict()
+    for board in boards1:
+        new_board = list()
         for line in board:
-            for num in line.split():
+            line = line.split()
+            win_cases = dict()
+            for num in line:
                 win_cases[num] = False
-            boards.append(win_cases)
-
-    for num in bingo:
-        for board in boards:
-            for n in board:
-                print(n)
+            new_board.append(win_cases)
+        boards.append(new_board)
 
     return score
     
